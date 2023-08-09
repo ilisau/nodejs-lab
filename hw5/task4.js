17cb03270fcbb056e7042ba2471f4301b781dd3b
Ilya Lisov <ilya.lisov.yt@gmail.com>
2023-08-09 09:49:41 +0300

    #20 Implement Task 4

---
+++ hw5/task4.js
@@ -0,0 +1,84 @@
+function getArrayIntersection(array1, array2) {
+    let result = []
+    let array1sorted = array1.sort()
+    let array2sorted = array2.sort()
+    let pos = 0
+    for (let i = 0; i < array1sorted.length; i++) {
+        for (let j = pos; j < array2sorted.length; j++) {
+            if (array1sorted[i] === array2sorted[j]) {
+                pos = j
+                result.push(array1sorted[i])
+                break
+            } else if (array1sorted[i] < array2sorted[j]) {
+                break
+            }
+        }
+    }
+    return result
+}
+
+function getArrayUnion(array1, array2) {
+    return [...new Set([...new Set(array1), ...new Set(array2)])]
+}
+
+// TESTS GET ARRAY INTERSECTION
+
+{
+    let array1 = [1, 2, 3, 4, 5, 6]
+    let array2 = [2, 3, 4, 5]
+    let result = getArrayIntersection(array1, array2)
+
+    console.log(result)
+}
+{
+    let array1 = [1, 2, 3, 4, 5, 6]
+    let array2 = [2, 3, 4, 5]
+    let result = getArrayIntersection(array2, array1)
+
+    console.log(result)
+}
+{
+    let array1 = [7]
+    let array2 = [2, 3, 4, 5]
+    let result = getArrayIntersection(array2, array1)
+
+    console.log(result)
+}
+{
+    let array1 = [5]
+    let array2 = [2, 3, 4, 5]
+    let result = getArrayIntersection(array2, array1)
+
+    console.log(result)
+}
+
+// TESTS GET ARRAY UNION
+
+{
+    let array1 = [1, 2, 3, 4, 5, 6]
+    let array2 = [2, 3, 4, 5]
+    let result = getArrayUnion(array1, array2)
+
+    console.log(result)
+}
+{
+    let array1 = [1, 2, 3, 4, 5, 6]
+    let array2 = [2, 3, 4, 5]
+    let result = getArrayUnion(array2, array1)
+
+    console.log(result)
+}
+{
+    let array1 = [7]
+    let array2 = [2, 3, 4, 5]
+    let result = getArrayUnion(array1, array2)
+
+    console.log(result)
+}
+{
+    let array1 = [5, 2, 3, 3, 5]
+    let array2 = [2, 3, 4, 5]
+    let result = getArrayUnion(array1, array2)
+
+    console.log(result)
+}
