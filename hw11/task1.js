a5bde82b3df45645a5a2c5beb187a0f1759d6747
Ilya Lisov <ilya.lisov.yt@gmail.com>
2023-09-13 12:31:04 +0300

    #42 Implement Queue

--- hw11/task1.js
+++ hw11/task1.js
@@ -109,7 +109,47 @@ class MinMaxStack {
 }
 
 class Queue {
-	// Implement methods for enqueue, dequeue, peek...
+	#items;
+	#frontIndex;
+	#backIndex;
+
+	/**
+	 * Creates object and initialize array, read and insert indices.
+	 */
+	constructor() {
+		this.#items = [];
+		this.#frontIndex = 0;
+		this.#backIndex = 0;
+	}
+
+	/**
+	 * Pushes item to the end of queue.
+	 * @param item
+	 */
+	enqueue(item) {
+		this.#items[this.#backIndex++] = item;
+	}
+
+	/**
+	 * Removes item from the start of queue.
+	 */
+	dequeue() {
+		if (this.#frontIndex !== this.#backIndex) {
+			this.#items[this.#frontIndex] = null;
+			this.#frontIndex++;
+		}
+	}
+
+	/**
+	 * Returns first item in queue or null if queue is empty.
+	 * @returns {*|null}
+	 */
+	peek() {
+		if (this.#frontIndex === this.#backIndex) {
+			return null;
+		}
+		return this.#items[this.#frontIndex];
+	}
 }
 
 class BinaryTree {
@@ -127,7 +167,7 @@ class LinkedList {
 // Implement Min/Max Stack, Binary Search Tree, Graph Algorithms...
 // Demonstrate usage and provide documentation...
 
-// TESTS
+// STACK TESTS
 
 {
 	let stack = new Stack();
@@ -179,4 +219,37 @@ class LinkedList {
 
 	console.log(stack.min() === 7)
 	console.log(stack.max() === 10)
+}
+
+// QUEUE TESTS
+
+{
+	let queue = new Queue();
+	queue.enqueue(1);
+	queue.enqueue(2);
+	queue.enqueue(3);
+	queue.enqueue(4);
+
+	console.log(queue.peek() === 1);
+}
+{
+	let queue = new Queue();
+	queue.enqueue(1);
+	queue.enqueue(2);
+	queue.dequeue();
+	queue.dequeue();
+	queue.enqueue(3);
+	queue.enqueue(4);
+
+	console.log(queue.peek() === 3);
+}
+{
+	let queue = new Queue();
+	queue.enqueue(1);
+	queue.dequeue();
+	queue.dequeue();
+	queue.dequeue();
+	queue.enqueue(3);
+
+	console.log(queue.peek() === 3);
 }
